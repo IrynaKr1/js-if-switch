48127b5a1b4b84386bb65fd6ae7230ff8d3166d9
IrynaKr1 <krutsiryna@gmail.com>
2025-05-29 19:21:34 +0200

    Add first task from if section

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// 1
+const isDay = true;
+
+if (isDay) {
+  document.write(`
+        <body style="background-color: #f0f0f0; color: #222; font-size: 24px; padding: 20px;">
+          Світла тема
+        </body>
+      `);
+} else {
+  document.write(`
+        <body style="background-color: #222; color: #f0f0f0; font-size: 24px; padding: 20px;">
+          Темна тема
+        </body>
+      `);
+}
